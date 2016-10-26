880ee0361b008e81dace697a04f5e48465cc952c
Vinicius Albino <viniciusmouralbino@gmail.com>
2016-10-25 23:56:31 -0200

    Interface for video recording

--- tvMed/tvMed/MoviePlayerController.swift
+++ tvMed/tvMed/MoviePlayerController.swift
@@ -9,11 +9,44 @@
 import Foundation
 import MediaPlayer
 
+enum status: String {
+    case Idle = "idle"
+    case Recording = "recording"
+    case Paused = "paused"
+    
+    var image:UIImage {
+        switch self {
+        case .Idle:
+            return UIImage(named: "record_icon")!
+        case .Recording:
+            return UIImage(named: "pause_icon")!
+        default:
+            return UIImage(named: "record_icon")!
+        }
+    }
+    
+    var statusLabel:String {
+        switch self {
+        case .Idle:
+            return "Iniciar gravação"
+        case .Recording:
+            return "Gravando..."
+        default:
+            return "Pausado"
+        }
+    }
+}
+
 class MoviePlayerController: UIViewController {
+    
+    @IBOutlet weak var recordButton:UIButton!
+    @IBOutlet weak var statusLabel:UILabel!
     
     @IBOutlet weak var container:UIView!
     var video:Movie?
     var containedViewController:UIViewController?
+    var player:AVPlayer?
+    var currentStatus = status.Idle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,21 +57,35 @@ class MoviePlayerController: UIViewController {
         self.loadVideo()
     }
     
+    @IBAction func recordButtonPressed() {
+        switch self.currentStatus {
+        case .Idle:
+            self.currentStatus = .Recording
+        case .Recording:
+            self.currentStatus = .Paused
+        default:
+            self.currentStatus = .Recording
+        }
+        
+        self.recordButton.setImage(self.currentStatus.image, forState: .Normal)
+        self.statusLabel.text = self.currentStatus.statusLabel
+    }
+    
     func loadVideo() {
         guard let currentMovie = self.video, let viewController = self.containedViewController else {
             return
         }
         
-        let player: AVPlayer = AVPlayer(URL: currentMovie.movieURL)
+        self.player = AVPlayer(URL: currentMovie.movieURL)
         
         let playerPreviewView = PlayerPreviewView(frame: CGRectZero)
         viewController.view.addSubview(playerPreviewView)
-        playerPreviewView.player = player
+        playerPreviewView.player = self.player
         
         playerPreviewView.translatesAutoresizingMaskIntoConstraints = false
         viewController.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[subview]-0-|", options: .DirectionLeadingToTrailing, metrics: nil, views: ["subview": playerPreviewView]))
         viewController.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[subview]-0-|", options: .DirectionLeadingToTrailing, metrics: nil, views: ["subview": playerPreviewView]))
-        player.play()
+        self.player!.play()
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
